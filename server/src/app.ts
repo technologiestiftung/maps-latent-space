0a8eabc8abb542ef5857097bc3fab03680a0593b
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-04-07 16:22:17 +0200

    feature(noHardware): Explicitly use hardware Adds a flag to the script to define to use hardware controllers explicitly

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,20 +6,26 @@ import http from "http";
 import { Board, Sensor, SensorOption } from "johnny-five";
 import EventEmitter from "events";
 import { createClient } from "./tcp-client";
+import { config } from "./config";
 
 const app = express();
 class Notify extends EventEmitter {}
 const notify = new Notify();
 const HOST = "127.0.0.1";
 const SOCKET_PORT = 9999;
-const board = new Board({ repl: false, port: "/dev/ttyACM0" });
+
+let board: Board | undefined;
+
+if (config.useHardware) {
+  board = new Board({ repl: false, port: "/dev/ttyACM0" });
+}
 /**
  * Express specific stuff
  *
  */
 app.use(cors());
 const sensors: Sensor[] = [];
-const sensorValues: number[] = [0, 0, 0, 0, 0, 0];
+const sensorValues: number[] = [0, 0, 0, 0, 0, 0, 0];
 const pins = ["A0", "A1" /*, "A2", "A4", "A5", "A6"*/];
 
 app.use(express.static(path.resolve(__dirname, "../../client")));
@@ -69,7 +75,7 @@ notify.on("ml_update", () => {
     "4": sensorValues[3],
     "5": sensorValues[4],
     "6": sensorValues[5],
-    "7": 0,
+    "7": sensorValues[6],
     "8": 0,
     "9": id
   };
@@ -99,6 +105,20 @@ wss.on("connection", function connection(ws) {
    */
   ws.on("message", function incoming(data) {
     console.info(`INFO:FRONTEND: Data received: ${data}`);
+    try {
+      const json = JSON.parse(data.toString());
+      if (json.sensorValues) {
+        for (let i = 0; i < 7; i++) {
+          sensorValues[i] = json.sensorValues[i] as number;
+        }
+        notify.emit("ml_update");
+        if (sensorValues[6] === 1) {
+          sensorValues[6] = 0;
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
   });
   /**
    * all websocket clients
@@ -132,34 +152,36 @@ wss.on("connection", function connection(ws) {
   // });
 });
 
-/**
- * The arduino board is ready
- *
- */
-board.on("ready", () => {
-  pins.forEach(pin => {
-    const opts: SensorOption = {
-      // eslint-disable-next-line
-      // @ts-ignore
-      freq: 250 /* @ts-ignore */,
-      pin: pin,
-      /* types dont are conform to docs */
-      threshold: 5 /* types dont are conform to docs */
-    };
+if (config.useHardware && board) {
+  /**
+   * The arduino board is ready
+   *
+   */
+  board.on("ready", () => {
+    pins.forEach(pin => {
+      const opts: SensorOption = {
+        // eslint-disable-next-line
+        // @ts-ignore
+        freq: 250 /* @ts-ignore */,
+        pin: pin,
+        /* types dont are conform to docs */
+        threshold: 5 /* types dont are conform to docs */
+      };
 
-    sensors.push(new Sensor(opts));
-    sensors.forEach((sensor, i) => {
-      sensor.on("change", () => {
-        console.info(
-          `INFO:SENSOR: Sensor A${sensor.pin} changed`,
-          sensor.fscaleTo(-1, 1)
-        );
-        sensorValues[i] = sensor.fscaleTo(-1, 1);
-        notify.emit("frontend_values");
-        // if (mlIsProcessing === false) {
-        notify.emit("ml_update");
-        // }
+      sensors.push(new Sensor(opts));
+      sensors.forEach((sensor, i) => {
+        sensor.on("change", () => {
+          console.info(
+            `INFO:SENSOR: Sensor A${sensor.pin} changed`,
+            sensor.fscaleTo(-1, 1)
+          );
+          sensorValues[i] = sensor.fscaleTo(-1, 1);
+          notify.emit("frontend_values");
+          // if (mlIsProcessing === false) {
+          notify.emit("ml_update");
+          // }
+        });
       });
     });
   });
-});
+}
